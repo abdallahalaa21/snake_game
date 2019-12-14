1c9786966d4ddd8cc862030ef1763628899eebe0
abdallahalaa21 <abdallah.bodi@gmail.com>
2019-12-14 23:41:55 +0200

    handle mobile feature

--- index.js
+++ index.js
@@ -1,53 +1,106 @@
 const canvas = document.getElementById("gamePlay");
+const scoreEle = document.getElementById("score");
+if (innerWidth < 500) {
+  canvas.width = window.screen.width - 10;
+}
+
+if (innerHeight < 500) {
+  canvas.height = window.screen.height - 100;
+}
 const ctx = canvas.getContext("2d");
 const scale = 10;
 ctx.scale(scale, scale);
-const scoreEle = document.getElementById("score");
 let dir, fruit, snake, totalScore;
+let x1, y1, x2, y2;
+
+const directions = {
+  down: { x: 0, y: 1 },
+  up: { x: 0, y: -1 },
+  right: { x: 1, y: 0 },
+  left: { x: -1, y: 0 }
+};
 
 const initialState = () => {
-  dir = { x: 1, y: 0 };
-  fruit = { x: 15, y: 0 };
+  dir = directions.right;
   snake = [
     { x: 4, y: 0 },
     { x: 3, y: 0 },
     { x: 2, y: 0 },
     { x: 1, y: 0 },
-    { x: 0, y: 0 },
+    { x: 0, y: 0 }
   ];
+  makeFruit();
   totalScore = 0;
   scoreEle.innerHTML = 0;
 };
 
 window.addEventListener("keydown", e => {
   if (e.keyCode === 40 && dir.y !== -1) {
-    dir = { x: 0, y: 1 };
+    dir = directions.down;
   }
   if (e.keyCode === 38 && dir.y !== 1) {
-    dir = { x: 0, y: -1 };
+    dir = directions.up;
   }
   if (e.keyCode === 39 && dir.x !== -1) {
-    dir = { x: 1, y: 0 };
+    dir = directions.right;
   }
   if (e.keyCode === 37 && dir.x !== 1) {
-    dir = { x: -1, y: 0 };
+    dir = directions.left;
   }
 });
 
+window.addEventListener("touchstart", e => {
+  handleMobile(e);
+});
+
+window.addEventListener("touchend", e => {
+  handleMobile(e);
+});
+
+const handleMobile = e => {
+  if (e && e.touches[0] && e.touches[0].screenX) {
+    x1 = e.touches[0].screenX;
+    y1 = e.touches[0].screenY;
+  }
+  if (e && e.changedTouches[0] && e.changedTouches[0].screenX) {
+    x2 = e.changedTouches[0].screenX;
+    y2 = e.changedTouches[0].screenY;
+  }
+  if (Math.abs((x1 - x2) / (y1 - y2)) > 1) {
+    if (x1 - x2 > 0 && dir.x !== 1) {
+      // console.log("left");
+      dir = directions.left;
+    }
+    if (x1 - x2 < 0 && dir.x !== -1) {
+      // console.log("right");
+      dir = directions.right;
+    }
+  }
+  if (Math.abs((y1 - y2) / (x1 - x2)) > 1) {
+    if (y1 - y2 > 0 && dir.y !== 1) {
+      // console.log("up");
+      dir = directions.up;
+    }
+    if (y1 - y2 < 0 && dir.y !== -1) {
+      // console.log("down");
+      dir = directions.down;
+    }
+  }
+};
+
 const move = () => {
   snake.unshift({ x: snake[0].x + dir.x, y: snake[0].y + dir.y });
-
-  if (snake[0].x == canvas.width / scale) {
+  if (snake[0].x == Math.floor(canvas.width / scale)) {
     snake[0].x = 0;
   }
-  if (snake[0].y == canvas.height / scale) {
+  if (snake[0].y == Math.floor(canvas.height / scale)) {
     snake[0].y = 0;
   }
   if (snake[0].x < 0) {
-    snake[0].x = canvas.width / scale - 1;
+    snake[0].x = Math.floor(canvas.width / scale) - 1;
   }
   if (snake[0].y < 0) {
-    snake[0].y = canvas.height / scale - 1;
+    snake[0].y = Math.floor(canvas.height / scale) - 1;
   }
 };
 
@@ -61,8 +114,8 @@ const draw = () => {
 
 const makeFruit = () => {
   fruit = {
-    x: Math.floor(Math.random() * 30),
-    y: Math.floor(Math.random() * 30),
+    x: Math.floor(Math.random() * (canvas.width / scale)),
+    y: Math.floor(Math.random() * (canvas.height / scale))
   };
   snake.forEach(({ x, y }) => {
     if (fruit.x === x && fruit.y === y) {
@@ -83,7 +136,7 @@ const eating = () => {
 };
 
 const checkDied = () => {
-  for (let i = 4; i < snake.length; i++) {
+  for (let i = 3; i < snake.length; i++) {
     if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
       alert("dieeed");
       initialState();
